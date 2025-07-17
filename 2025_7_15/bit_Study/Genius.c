65496d0b3bc4ba6ec962fffbb8c7f2a53637b0da
qlzzry <eqinlu@petalmail.com>
2025-07-17 21:21:13 +0800

    2025/7/15_C代码

---
+++ 2025_7_15/bit_Study/Genius.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	int m;
+	m = 0;
+
+	printf("请输入测试的智商结果：");
+	scanf("%d", &m);
+	if(m >= 140)
+		printf("Gemius!!");
+	else
+		printf("no!");
+
+	return 0;
+
+}
---
+++ 2025_7_15/bit_Study/计算带余除法.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	a = b = 0;
+
+	printf("请输入两个数值：");
+	scanf("%d %d", &a, &b);
+	printf("%d除以%d=%d,余上%d", a, b, a / b, a % b);
+
+	return 0;
+}
