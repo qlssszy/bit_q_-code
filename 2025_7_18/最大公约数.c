abf3f5af3eedf6b143dedfc179d2f40adde01462
qlzzry <eqinlu@petalmail.com>
2025-07-19 12:49:47 +0800

    2025/7/19 C代码

---
+++ 2025_7_18/最大公约数.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int a, b;
+	int gcd = 1;
+
+	printf("请输入两个整数：");
+	scanf("%d %d", &a, &b);
+	if (a % b == 0)
+	{
+		printf("最大公约数为%d\n", b);
+	}
+	else if (b % a == 0)
+	{
+		printf("最大公约数为%d\n", a);
+	}
+	else
+	{
+		for (i = 1; i <= a && i <= b; i++)
+		{
+			if (a % i == 0 && b % i == 0)
+			{
+				gcd = i; 
+			}
+		}
+		printf("最大公约数为%d", gcd);
+
+	}
+	
+
+	return 0;
+}
