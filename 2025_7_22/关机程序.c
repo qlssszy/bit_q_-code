d8dbf8ad713ef1975e44e6998d9d72fbc2b628d9
qlzzry <eqinlu@petalmail.com>
2025-07-22 19:38:06 +0800

    关机程序

---
+++ 2025_7_22/关机程序.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char input[20];
+
+again:
+	system("shutdown -s -t 60");//使用系统关机命令
+	printf("请注意你的电脑将在60S内关机，输入“我是猪”才能取消关机:");
+	scanf("%s", input);
+	if (strcmp("我是猪", input) == 0) //使用库函数判断字符串是否相等
+		system("shutdown -a");
+	else
+		goto again;  //goto语句跳转，循环判断
+
+	return 0;
+}
