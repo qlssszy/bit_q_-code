3c95f3f39d7c1ac4a7542badaff16e8e8cefb4be
qlzzry <eqinlu@petalmail.com>
2025-07-24 16:46:41 +0800

    github第二次提交测试

---
+++ 2025_7_24/github2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello github!!!");
+	printf("hello github!!!");
+	printf("hello github!!!");
+	printf("hello github!!!");
+	printf("hello github!!!");
+
+	return 0;
+}
